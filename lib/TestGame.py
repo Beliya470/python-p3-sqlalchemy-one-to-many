a7254b134a6b61cf80babfdf633f88e947356578
Beliya470 <anneanziay78@gmail.com>
2023-09-05 16:26:29 +0300

    Completed Assignment

--- lib/TestGame.py
+++ lib/TestGame.py
@@ -16,48 +16,51 @@ def db_session():
 class TestGame:
     '''Class Game in models.py'''
 
-    # start session, reset db
-    engine = create_engine(SQLITE_URL)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    @classmethod
-    def setup_class(cls):
-        # add test data
-        cls.mario_kart = Game(
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_data(self, db_session):
+        self.mario_kart = Game(
             title="Mario Kart",
             platform="Switch",
             genre="Racing",
             price=60
         )
 
-        cls.session.add(cls.mario_kart)
-        cls.session.commit()
+        db_session.add(self.mario_kart)
+        db_session.commit()
 
         mk_review_1 = Review(
             score=10,
             comment="Wow, what a game",
-            game_id=cls.mario_kart.id
+            game_id=self.mario_kart.id
         )
 
         mk_review_2 = Review(
             score=8,
             comment="A classic",
-            game_id=cls.mario_kart.id
+            game_id=self.mario_kart.id
         )
 
-        cls.session.bulk_save_objects([mk_review_1, mk_review_2])
-        cls.session.commit()
+        db_session.bulk_save_objects([mk_review_1, mk_review_2])
+        db_session.commit()
 
     def test_game_has_correct_attributes(self):
         '''has attributes "id", "title", "platform", "genre", "price".'''
-        assert all(hasattr(self.mario_kart, attr) for attr in ["id", "title", "platform", "genre", "price"])
+        assert(
+            all(
+                hasattr(
+                    self.mario_kart, attr
+                ) for attr in [
+                    "id",
+                    "title",
+                    "platform",
+                    "genre",
+                    "price"
+                ]))
 
     def test_has_associated_reviews(self):
         '''has two reviews with scores 10 and 8.'''
-        assert (
+        assert(
             len(self.mario_kart.reviews) == 2 and
             self.mario_kart.reviews[0].score == 10 and
             self.mario_kart.reviews[1].score == 8
         )
-
--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
 package_dir = '/'.join(os.path.abspath(os.path.dirname(__file__)).split('/')[0:-1])
 db_dir = os.path.join(package_dir, 'one_to_many.db')
 SQLITE_URL = ''.join(['sqlite:///', db_dir])
+
+# Initialize the database engine and create tables
+engine = create_engine(SQLITE_URL)
+Base.metadata.create_all(engine)
+
+# Create a sessionmaker
+Session = sessionmaker(bind=engine)
 
 def pytest_itemcollected(item):
     par = item.parent.obj
